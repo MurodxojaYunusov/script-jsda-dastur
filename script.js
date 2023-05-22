65693f448b06bf4731ca3323e18e98289f73cfd1
MurodxojaYunusov <murodxojayunusov492@gmail.com>
2023-05-22 16:41:16 +0500

    sadasd

---
+++ script.js
@@ -0,0 +1,24 @@
+// find the largest among three numbers
+
+let num1 = 100;
+let num2 = 200;
+let num3 = 300;
+let largest
+
+//check the condition
+
+if ((num1 >= num2 && num1 >= num3 ) ) {
+    largest = num1;
+    
+}
+else if(num2 >= num1 && num2 >= num3){
+    largest = num2
+}
+else{
+    largest = num3;
+}
+
+//display the result 
+console.log('The largesT number is larges ')
+
+
